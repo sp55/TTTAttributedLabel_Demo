e0c523ada9cff062709b202e3ecb9d05b331c5f6
admin <sp0302@126.com>
2016-09-02 23:08:54 +0800

    一个字符串包含的全部字符串
    一个字符串包含的全部字符串

--- TTTAttributedLabel_Demo/ViewController.m
+++ TTTAttributedLabel_Demo/ViewController.m
@@ -22,11 +22,8 @@
     [super viewDidLoad];
 
     //TTTAttributedLabe作为UILabel的替代，可以轻松的渲染可变字符串，文中中加入嵌入链接，手机号，时间都可以得到相对应的处理
-    
-
-    
-    NSString           *text  = @"美国职业篮球联赛（National Basketball Association，简称NBA，中文简称“美职篮”）于1946年6月6日在纽约成立(FlyElephant)，是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育...";
-    TTTAttributedLabel *label = [[TTTAttributedLabel alloc] initWithFrame:CGRectMake(20, 80, 300, 100)];
+    NSString           *text  = @"美国职业篮球联赛百度是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育美国职业篮球联赛百度是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育美国职业篮球联赛百度是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育美国职业篮球联赛百度是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育美国职业篮球联赛百度是由北美三十支队伍组成的男子职业篮球联盟，美国四大职业体育";
+    TTTAttributedLabel *label = [[TTTAttributedLabel alloc] initWithFrame:CGRectMake(20, 80, 300, 500)];
     label.textColor = [UIColor whiteColor];
     NSMutableAttributedString *atributeStr = [[NSMutableAttributedString alloc] initWithString:text attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor],NSFontAttributeName :[UIFont systemFontOfSize:14]}];
     //可以传属性字符串
@@ -37,12 +34,32 @@
     //链接的属性字符串
     label.linkAttributes       = @{NSForegroundColorAttributeName:[UIColor blackColor], NSFontAttributeName:[UIFont systemFontOfSize:16.0f]};
     label.activeLinkAttributes = @{NSForegroundColorAttributeName:[UIColor orangeColor], NSFontAttributeName:[UIFont systemFontOfSize:16.0f]};
+    
+    
+    
     //那个是可以点击的链接
-    NSRange serviceRange = [text rangeOfString:@"NBA"];
+    //    NSRange serviceRange = [text rangeOfString:@"美国"];
+    //    label.delegate = self;
+    //    [self.view addSubview:label];
+    //    [label addLinkToURL:[NSURL URLWithString:@"http://china.nba.com"] withRange:serviceRange];
+    
+    
+    
+    NSRange searchRange = NSMakeRange(0, [text length]);
+    NSRange range;
+    while ((range = [text rangeOfString:@"美国" options:0 range:searchRange]).location != NSNotFound) {
+        searchRange = NSMakeRange(NSMaxRange(range), [text length] - NSMaxRange(range));
+        [label addLinkToURL:[NSURL URLWithString:@"http://china.nba.com"] withRange:range];
+    }
+    
+    
+    
+    
+    
+    NSRange serviceRange2 = [text rangeOfString:@"百度"];
     label.delegate = self;
     [self.view addSubview:label];
-    [label addLinkToURL:[NSURL URLWithString:@"http://china.nba.com"] withRange:serviceRange];
-    
+    [label addLinkToURL:[NSURL URLWithString:@"http://www.baidu.com"] withRange:serviceRange2];
 }
 //实现TTTAttributedLabelDelegate协议:
 -(void)attributedLabel:(TTTAttributedLabel *)label didSelectLinkWithURL:(NSURL *)url {
